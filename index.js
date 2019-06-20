2ce713f356701973225bb8dd1f4749deae66a995
ampersky <gasttello@gmail.com>
2019-06-20 03:17:15 +0300

    День_1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const searchForm = document.querySelector('#search-form');
-
+const movie = document.querySelector('#movies');
 
 searchForm.addEventListener('submit', apiSearch);
 
@@ -12,18 +12,38 @@ function apiSearch(event) {
     sendRequest('GET', server)
 }
 
+
 const sendRequest = function (method, url) {
     let request = new XMLHttpRequest();
-    console.log(request);
+    //console.log(request);
     request.open(method, url);
     request.send();
 
  request.addEventListener('readystatechange', () => {
-     if(request.readyState !=4) {return 'Loading'}
-     else if (request.readyState == 4) {
+     let inner='';
+     let img_url = 'https://image.tmdb.org/t/p/w1280';
 
+     if(request.readyState !==4) return 'Loading';
+     if (request.readyState !== 200) {
+         movie.innerHTML = `Ошибка${request.status}`}
+
+         let response = request.responseText;
+         let output = JSON.parse(response);
+         console.log(output.results);
+
+         output.results.forEach((item)=>{
+            let nameItem=item.name||item.title,
+                imgPoster = item.backdrop_path ? (`${img_url}${item.backdrop_path}`):(`${img_url}${item.poster_path}`),
+                release = item.release_date;
+            inner+=`<div class="col-12">${nameItem}
+                           <div>Дата релиза: ${release}</div>
+                          <img src="${imgPoster}" alt=""/>
+                     <div/>`;
+            movie.innerHTML=inner;
+        })
      }
- })
+ )
 
 };
+
 

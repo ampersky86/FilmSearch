01b3a28d0a95f83dd313da1db3b3ffc537cd5685
ampersky <gasttello@gmail.com>
2019-06-22 19:14:11 +0300

    fix_2

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ searchForm.addEventListener('submit', apiSearch);
 
 function apiSearch(event) {
     event.preventDefault();
-    const searchText = document.querySelector('.form-control').value;
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=a60d1f2c9be5f3b376503e6121eaac60&language=ru&query='+searchText
-    sendRequest('GET', server)
+    let searchText = document.querySelector('.form-control').value;
+    let server = 'https://ap.themoviedb.org/3/search/multi?api_key=a60d1f2c9be5f3b376503e6121eaac60&language=ru&query='+ searchText;
+    sendRequest('GET', server);
 }
 
 
@@ -17,18 +17,18 @@ const sendRequest = function (method, url) {
     let request = new XMLHttpRequest();
     request.open(method, url);
     request.send();
-
- request.addEventListener('readystatechange', () => {
-     let inner='',
-      img_url = 'https://image.tmdb.org/t/p/w1280',
-      response = request.responseText,
-      output =JSON.parse(response);
-
+    request.addEventListener('readystatechange', () => {
      if(request.readyState !==4)
-         return 'Loading';
-     if (request.readyState !== 200) {
-         movie.innerHTML = `Ошибка${request.status}`}
+         return ;
+     if (request.status !== 200) {
+         movie.innerHTML = `Ошибка${request.status}`;
+         return;
+     }
 
+
+     let inner='';
+     let img_url = 'https://image.tmdb.org/t/p/w1280';
+     let output =JSON.parse(request.responseText);
 
          console.log(output);
 
@@ -50,7 +50,6 @@ const sendRequest = function (method, url) {
         })
      }
  )
-
 };
 
 

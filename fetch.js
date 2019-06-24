c42149306aac080105a974b227288127f58343b3
ampersky <gasttello@gmail.com>
2019-06-24 03:12:16 +0300

    Добавлен fetch

--- fetch.js
+++ fetch.js
@@ -8,15 +8,22 @@ searchForm.addEventListener('submit', apiSearch);
 function apiSearch(event) {
     event.preventDefault();
     const searchText = document.querySelector('.form-control').value;
-    const server = 'https://api.themoviedb.org/3/search/multi?api_key=a60d1f2c9be5f3b376503e6121eaac60&language=ru&query='+searchText
-    sendRequest('GET', server)
+    const server = 'https://api.themoviedb.org/3/search/multi?api_key=a60d1f2c9be5f3b376503e6121eaac60&language=ru&query='+searchText;
+    movie.innerHTML='Загрузка';
+
+    fetch(server)
         .then(function (result) {
-            let output = JSON.parse(result);
+            if (result.status!==200) {
+                return Promise.reject(new Error(result.status));
+            }
+            return result.json();
+        })
+        .then(function (output) {
             let inner = '';
-            let img_url = 'https://image.tmdb.org/t/p/w1280';
+            let img_url = 'https://image.tmdb.org/t/p/w500';
             output.results.forEach((item)=>{
                 let nameItem=item.name||item.title;
-                let imgPoster = `${img_url}${item.poster_path}`;// ? (`${img_url}${item.backdrop_path}`):(`${img_url}${item.poster_path}`);
+                let imgPoster = `${img_url}${item.poster_path}`;//item.poster_path ? (`${img_url}${item.poster_path}`):(`${img_url}${item.backdrop_path}`);
                 let overview = item.overview;
                 inner+=`<div class="row">
                         <div class="card" style="width:18rem;">
@@ -29,34 +36,11 @@ function apiSearch(event) {
                         </div>    
                     </div>`;
                 movie.innerHTML=inner;
-                })
             })
-        .catch(function (error) {
-            console.log(`Ошибка - ${error}`);
+        })
+        .catch(function (reason) {
+            console.log(`Ошибка - ${reason}`);
             movie.innerHTML='Что-то пошло не так';
         });
     }
-
-function sendRequest (method, url) {
-    return new Promise(function (resolve, reject) {
-        let request = new XMLHttpRequest();
-        request.open(method,url);
-        request.addEventListener('load', ()=>{
-            if (request.status!==200) {
-                reject({status: request.status});
-                return;
-            }
-            resolve(request.response);
-        });
-
-        request.addEventListener('error', ()=>{
-            reject({status: request.status});
-        });
-        request.send();
-    })
-}
-
-
-
-
 

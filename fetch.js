f84cc349ad1139915b0f97b66288f375cd1b6b7f
ampersky <gasttello@gmail.com>
2019-06-26 11:54:07 +0300

    День_3

--- fetch.js
+++ fetch.js
@@ -1,15 +1,18 @@
 const searchForm = document.querySelector('#search-form');
 const movie = document.querySelector('#movies');
 
+
 searchForm.addEventListener('submit', apiSearch);
-
-
 
 function apiSearch(event) {
     event.preventDefault();
     const searchText = document.querySelector('.form-control').value;
+    if (searchText.trim().length === 0){
+        movie.innerHTML='<h2 class="col-12 text-center text-danger">Поле поиска не должно быть пустым</h2>';
+        return;
+    }
     const server = 'https://api.themoviedb.org/3/search/multi?api_key=a60d1f2c9be5f3b376503e6121eaac60&language=ru&query='+searchText;
-    movie.innerHTML='Загрузка';
+    movie.innerHTML = '<div class="lds-css ng-scope"> <div style="width:100%;height:100%" class="lds-cube"><div></div><div></div><div></div><div></div></div>';
 
     fetch(server)
         .then(function (result) {
@@ -21,13 +24,17 @@ function apiSearch(event) {
         .then(function (output) {
             let inner = '';
             let img_url = 'https://image.tmdb.org/t/p/w500';
+            console.log(output.results.length);
+            if (output.results.length === 0){
+                inner = '<h2 class="col-12 text-center text-info">По вашему запросу ничего не найдено</h2>';
+            }
             output.results.forEach((item)=>{
-                let nameItem=item.name||item.title;
-                let imgPoster = `${img_url}${item.poster_path}`;//item.poster_path ? (`${img_url}${item.poster_path}`):(`${img_url}${item.backdrop_path}`);
+                let nameItem = item.name||item.title;
+                let poster = item.poster_path ? (`${img_url}${item.poster_path}`):'./img/not_poster.jpg';
                 let overview = item.overview;
                 inner+=`<div class="row">
                         <div class="card" style="width:18rem;">
-                            <img class="card-img-top" src="${imgPoster}" alt="Card image cap">
+                            <img class="card-img-top" src="${poster}" alt="${nameItem}">
                             <div class="card-body">
                                 <h5 class="card-title">${nameItem}</h5>
                                 <p class="card-text">${overview}</p>
@@ -35,8 +42,8 @@ function apiSearch(event) {
                             </div>
                         </div>    
                     </div>`;
-                movie.innerHTML=inner;
-            })
+            });
+            movie.innerHTML=inner;
         })
         .catch(function (reason) {
             console.log(`Ошибка - ${reason}`);
